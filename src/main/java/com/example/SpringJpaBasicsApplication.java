671fec20d39394d2535113b0b04a8d3db1ac6987
Shivam Bhilarkar <shivambhilarkar@gmail.com>
2022-11-21 20:25:56 +0530

    Update SpringJpaBasicsApplication.java

--- src/main/java/com/example/SpringJpaBasicsApplication.java
+++ src/main/java/com/example/SpringJpaBasicsApplication.java
@@ -13,55 +13,5 @@ public class SpringJpaBasicsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJpaBasicsApplication.class, args);
 	}
-	
-	/*
-	@Bean
-    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
-        return args -> {
-        	
-            Student maria = new Student(
-                    "Maria",
-                    "Jones",
-                    "maria.jones@amigoscode.edu",
-                    21
-            );
-
-            Student maria2 = new Student(
-                    "Maria",
-                    "Jones",
-                    "maria2.jones@amigoscode.edu",
-                    25
-            );
-
-            Student ahmed = new Student(
-                    "Ahmed",
-                    "Ali",
-                    "ahmed.ali@amigoscode.edu",
-                    18
-            );
-
-            studentRepository.saveAll(List.of(maria, ahmed, maria2));
-
-            studentRepository
-                    .findStudentByEmail("ahmed.ali@amigoscode.edu")
-                    .ifPresentOrElse(
-                            System.out::println,
-                            () -> System.out.println("Student with email ahmed.ali@amigoscode.edu not found"));
-
-            studentRepository.selectStudentWhereFirstNameAndAgeGreaterOrEqual(
-                    "Maria",
-                    21
-            ).forEach(System.out::println);
-
-            studentRepository.selectStudentWhereFirstNameAndAgeGreaterOrEqualNative(
-                    "Maria",
-                    21
-            ).forEach(System.out::println);
-
-            System.out.println("Deleting Maria 2");
-            System.out.println(studentRepository.deleteStudentById(3L));
-        };
-	}
-	*/
 
 }
