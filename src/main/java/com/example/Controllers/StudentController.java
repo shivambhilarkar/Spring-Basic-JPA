78e276835871b73d64802350f3967c2a106bf54f
Shivam Bhilarkar <shivambhilarkar@gmail.com>
2022-11-21 20:23:08 +0530

    Basic JPA

---
+++ src/main/java/com/example/Controllers/StudentController.java
@@ -0,0 +1,60 @@
+package com.example.Controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.SpringJPABasics.Student;
+import com.example.SpringJPABasics.StudentRepository;
+
+@RestController
+public class StudentController {
+	
+	private final StudentRepository studentRepository;
+	
+	@Autowired
+	public StudentController(StudentRepository studentRepository) {
+		this.studentRepository = studentRepository;
+	}
+	
+	@GetMapping
+	@RequestMapping("api/homescreen")
+	public String homescreen() {
+		System.out.println("Welcome to homescreen");
+		return "Welcome to Student Managment System";
+	}
+	
+	@GetMapping
+	@RequestMapping("api/getall")
+	public List<Student> single() {
+		//to get all the rows from the database
+		return studentRepository.findAll();
+	}
+	
+	@PostMapping
+	@RequestMapping("api/addstudents")
+	public void addStudents() {
+		//dummy objects
+		Student shivam = new Student("Shivam","Bhilarkar","shivam@gmail.com",23 );
+		Student vaishnavi = new Student("Vaishnavi","Bhilarkar","vaishnavi@gmail.com",23 );		
+		Student manish = new Student("Manish","Jadhav","manish@gmail.com",23 );
+		
+		//use the JPA inbuilt method to save list
+		studentRepository.saveAll(List.of(shivam, vaishnavi,manish));
+	}
+	
+	@GetMapping
+	@RequestMapping("api/getsingle")
+	public Object getsingle() {
+		//find by Id inbuild method
+		return studentRepository.findById((long) 1);
+	}
+	
+
+	
+
+}
---
+++ src/test/java/com/example/SpringJPABasics/SpringJpaBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringJPABasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
